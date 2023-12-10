907305c0b23d33c9185eadd64491740fcee5868b
shahin <shahinzam1402@gmail.com>
2023-12-11 00:28:06 +0330

    delete st.session

--- todo_web.py
+++ todo_web.py
@@ -24,7 +24,3 @@ for index,todo in enumerate(todos):
         del st.session_state[todo]
 
 add = st.text_input("enter todo", placeholder="Add new todo", on_change=add_todo, key="new_todo")
-
-
-st.session_state
-
